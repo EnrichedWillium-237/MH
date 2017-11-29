1dfc0bae3e4faca4c79ce3fdf91e2986ef576677
EnrichedWillium-235 <w955m639@ku.edu>
2017-11-28 21:14:42 -0600

    Update

--- macros/plotting/v1pT.C
+++ macros/plotting/v1pT.C
@@ -206,12 +206,12 @@ void v1pT()
 
 
     //-- v1even (N1MCm22SUB3 and N1MCp22SUB3) vs. pT for each eta-bin
-    TCanvas * cv1evenDiffPt_A_B_30_50 = new TCanvas("cv1evenDiffPt_A_B_30_50","cv1evenDiffPt_A_B_30_50",1100,700);
+    TCanvas * cv1evenDiffPt_A_B_30_50 = new TCanvas("cv1evenDiffPt_A_B_30_50","cv1evenDiffPt_A_B_30_50",1000,800);
     cv1evenDiffPt_A_B_30_50->Divide(4,3,0,0);
     TH1D * hv1evenDiffPt_A_B_30_50_tmp = new TH1D("hv1evenDiffPt_A_B_30_50_tmp", "", 50, 0, 12);
     hv1evenDiffPt_A_B_30_50_tmp->SetStats(0);
     hv1evenDiffPt_A_B_30_50_tmp->SetXTitle("p_{T} (GeV/c)");
-    hv1evenDiffPt_A_B_30_50_tmp->SetYTitle("v_{1}");
+    hv1evenDiffPt_A_B_30_50_tmp->SetYTitle("v_{1}^{even}");
     hv1evenDiffPt_A_B_30_50_tmp->GetXaxis()->SetTitleSize(0.06);
     hv1evenDiffPt_A_B_30_50_tmp->GetYaxis()->SetTitleSize(0.07);
     hv1evenDiffPt_A_B_30_50_tmp->GetXaxis()->SetTitleOffset(1.10);
@@ -219,12 +219,15 @@ void v1pT()
     hv1evenDiffPt_A_B_30_50_tmp->GetXaxis()->SetLabelSize(0.06);
     hv1evenDiffPt_A_B_30_50_tmp->GetYaxis()->SetLabelSize(0.06);
     hv1evenDiffPt_A_B_30_50_tmp->GetYaxis()->SetNdivisions(509);
+    hv1evenDiffPt_A_B_30_50_tmp->GetXaxis()->CenterTitle();
+    hv1evenDiffPt_A_B_30_50_tmp->GetYaxis()->CenterTitle();
     hv1evenDiffPt_A_B_30_50_tmp->GetYaxis()->SetRangeUser(-0.075, 0.22);
     for (int ebin = 0; ebin<netabins; ebin++) {
         TPad * padv1evenDiffPt_A_B_30_50 = (TPad *) cv1evenDiffPt_A_B_30_50->cd(ebin+1);
         if (gridlines) padv1evenDiffPt_A_B_30_50->SetGrid();
         if (ebin == 3 || ebin == 7 || ebin == 11) padv1evenDiffPt_A_B_30_50->SetRightMargin(0.02);
         if (ebin <=3) padv1evenDiffPt_A_B_30_50->SetTopMargin(0.02);
+        if (ebin <=3) padv1evenDiffPt_A_B_30_50->SetTopMargin(0.08);
         TH1D * hv1evenDiffPt_A_B_30_50 = (TH1D *) hv1evenDiffPt_A_B_30_50_tmp->Clone(Form("hv1evenDiffPt_A_B_30_50_%d",ebin));
         hv1evenDiffPt_A_B_30_50->Draw();
 
@@ -241,43 +244,52 @@ void v1pT()
         else vnA[0][0][12][ebin]->Draw("same");
 
         TPaveText * txv1evenDiffPt_A_B_30_50_ebin;
-        if (ebin>=8) txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.53, 0.21, 0.90, 0.33,"NDC");
-        else txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.53, 0.06, 0.88, 0.18,"NDC");
+        if (ebin == 0 || ebin == 4) txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.56, 0.05, 0.96, 0.16,"NDC");
+        else if (ebin>=1 && ebin<=3) txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.48, 0.05, 0.86, 0.16,"NDC");
+        else if (ebin == 8) txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.56, 0.25, 0.96, 0.36,"NDC");
+        else txv1evenDiffPt_A_B_30_50_ebin = new TPaveText(0.48, 0.25, 0.86, 0.36,"NDC");
         SetTPaveTxt(txv1evenDiffPt_A_B_30_50_ebin, 17);
         txv1evenDiffPt_A_B_30_50_ebin->AddText(Form("%1.1f < #eta < %1.1f",etabins[ebin],etabins[ebin+1]));
         txv1evenDiffPt_A_B_30_50_ebin->Draw();
     }
     cv1evenDiffPt_A_B_30_50->cd(1);
-    TPaveText * txv1evenDiffPt_A_B_30_50_cbin = new TPaveText(0.24, 0.73, 0.40, 0.93,"NDC");
-    SetTPaveTxt(txv1evenDiffPt_A_B_30_50_cbin, 17);
-    txv1evenDiffPt_A_B_30_50_cbin->AddText("N1MC22SUB3");
+    TPaveText * txv1evenDiffPt_A_B_30_50_cbin = new TPaveText(0.24, 0.68, 0.76, 0.87,"NDC");
+    SetTPaveTxt(txv1evenDiffPt_A_B_30_50_cbin, 18);
+    txv1evenDiffPt_A_B_30_50_cbin->AddText("PbPb #sqrt{s_{NN}} = 5.02 TeV");
     txv1evenDiffPt_A_B_30_50_cbin->AddText("30-50%");
     txv1evenDiffPt_A_B_30_50_cbin->Draw();
+
+    TPaveText * txv1evenDiffPt_A_B_30_50_cbin_cms = new TPaveText(0.18, 0.93, 0.58, 1.0,"NDC");
+    SetTPaveTxt(txv1evenDiffPt_A_B_30_50_cbin_cms, 18);
+    txv1evenDiffPt_A_B_30_50_cbin_cms->AddText("#bf{CMS} #it{Preliminary}");
+    txv1evenDiffPt_A_B_30_50_cbin_cms->Draw();
 
     cv1evenDiffPt_A_B_30_50->Print("figures/vN1MC22SUB3_Pt_30_50.png","png");
 
 
 
-    //-- v1odd (N1SUB3) vs. pT for each eta-bin
-    TCanvas * cv1oddDiffPt_30_50 = new TCanvas("cv1oddDiffPt_30_50","cv1oddDiffPt_30_50",1100,700);
+    //-- v1odd (N1SUB2) vs. pT for each eta-bin
+    TCanvas * cv1oddDiffPt_30_50 = new TCanvas("cv1oddDiffPt_30_50","cv1oddDiffPt_30_50",1000,800);
     cv1oddDiffPt_30_50->Divide(4,3,0,0);
-    TH1D * hv1oddDiffPt_30_50_tmp = new TH1D("hv1oddDiffPt_30_50_tmp", "", 50, 0, 12);
+    TH1D * hv1oddDiffPt_30_50_tmp = new TH1D("hv1oddDiffPt_30_50_tmp", "", 50, 0, 13);
     hv1oddDiffPt_30_50_tmp->SetStats(0);
     hv1oddDiffPt_30_50_tmp->SetXTitle("p_{T} (GeV/c)");
-    hv1oddDiffPt_30_50_tmp->SetYTitle("v_{1}");
+    hv1oddDiffPt_30_50_tmp->SetYTitle("v_{1}^{odd}");
     hv1oddDiffPt_30_50_tmp->GetXaxis()->SetTitleSize(0.06);
     hv1oddDiffPt_30_50_tmp->GetYaxis()->SetTitleSize(0.07);
     hv1oddDiffPt_30_50_tmp->GetXaxis()->SetTitleOffset(1.10);
-    hv1oddDiffPt_30_50_tmp->GetYaxis()->SetTitleOffset(1.10);
+    hv1oddDiffPt_30_50_tmp->GetYaxis()->SetTitleOffset(1.20);
     hv1oddDiffPt_30_50_tmp->GetXaxis()->SetLabelSize(0.06);
     hv1oddDiffPt_30_50_tmp->GetYaxis()->SetLabelSize(0.06);
+    hv1oddDiffPt_30_50_tmp->GetXaxis()->CenterTitle();
+    hv1oddDiffPt_30_50_tmp->GetYaxis()->CenterTitle();
     hv1oddDiffPt_30_50_tmp->GetYaxis()->SetNdivisions(509);
-    hv1oddDiffPt_30_50_tmp->GetYaxis()->SetRangeUser(-0.20, 0.20);
+    hv1oddDiffPt_30_50_tmp->GetYaxis()->SetRangeUser(-0.12, 0.12);
     for (int ebin = 0; ebin<netabins; ebin++) {
         TPad * padv1oddDiffPt_30_50 = (TPad *) cv1oddDiffPt_30_50->cd(ebin+1);
         if (gridlines) padv1oddDiffPt_30_50->SetGrid();
         if (ebin == 3 || ebin == 7 || ebin == 11) padv1oddDiffPt_30_50->SetRightMargin(0.02);
-        if (ebin <=3) padv1oddDiffPt_30_50->SetTopMargin(0.02);
+        if (ebin <=3) padv1oddDiffPt_30_50->SetTopMargin(0.08);
         TH1D * hv1oddDiffPt_30_50 = (TH1D *) hv1oddDiffPt_30_50_tmp->Clone(Form("hv1oddDiffPt_30_50_%d",ebin));
         hv1oddDiffPt_30_50->Draw();
 
@@ -296,30 +308,38 @@ void v1pT()
         vnAB[0][16][12][ebin]->SetMarkerStyle(21);
         vnAB[0][16][12][ebin]->SetMarkerSize(1.1);
 
-        vnA[0][16][12][ebin]->Draw("same");
-        vnB[0][16][12][ebin]->Draw("same");
+        // vnA[0][16][12][ebin]->Draw("same");
+        // vnB[0][16][12][ebin]->Draw("same");
         vnAB[0][16][12][ebin]->Draw("same");
 
         TPaveText * txv1oddDiffPt_30_50_ebin;
-        if (ebin>=8) txv1oddDiffPt_30_50_ebin = new TPaveText(0.53, 0.21, 0.90, 0.33,"NDC");
-        else txv1oddDiffPt_30_50_ebin = new TPaveText(0.53, 0.06, 0.88, 0.18,"NDC");
+        if (ebin == 0) txv1oddDiffPt_30_50_ebin = new TPaveText(0.23, 0.75, 0.59, 0.86,"NDC");
+        else if (ebin>=1 && ebin<=3) txv1oddDiffPt_30_50_ebin = new TPaveText(0.04, 0.75, 0.41, 0.86,"NDC");
+        else if (ebin == 4 || ebin == 8) txv1oddDiffPt_30_50_ebin = new TPaveText(0.23, 0.82, 0.59, 0.94,"NDC");
+        else txv1oddDiffPt_30_50_ebin = new TPaveText(0.04, 0.82, 0.41, 0.94,"NDC");
         SetTPaveTxt(txv1oddDiffPt_30_50_ebin, 17);
         txv1oddDiffPt_30_50_ebin->AddText(Form("%1.1f < #eta < %1.1f",etabins[ebin],etabins[ebin+1]));
         txv1oddDiffPt_30_50_ebin->Draw();
     }
-    cv1oddDiffPt_30_50->cd(2);
-    TPaveText * txv1oddDiffPt_30_50_cbin = new TPaveText(0.06, 0.81, 0.22, 0.93,"NDC");
-    SetTPaveTxt(txv1oddDiffPt_30_50_cbin, 17);
+    cv1oddDiffPt_30_50->cd(1);
+    TPaveText * txv1oddDiffPt_30_50_cbin = new TPaveText(0.24, 0.06, 0.76, 0.28,"NDC");
+    SetTPaveTxt(txv1oddDiffPt_30_50_cbin, 18);
+    txv1oddDiffPt_30_50_cbin->AddText("PbPb #sqrt{s_{NN}} = 5.02 TeV");
     txv1oddDiffPt_30_50_cbin->AddText("30-50%");
     txv1oddDiffPt_30_50_cbin->Draw();
 
+    TPaveText * txv1oddDiffPt_30_50_cbin_cms = new TPaveText(0.18, 0.93, 0.58, 1.0,"NDC");
+    SetTPaveTxt(txv1oddDiffPt_30_50_cbin_cms, 18);
+    txv1oddDiffPt_30_50_cbin_cms->AddText("#bf{CMS} #it{Preliminary}");
+    txv1oddDiffPt_30_50_cbin_cms->Draw();
+
     cv1oddDiffPt_30_50->cd(1);
-    TLegend * legv1oddDiffPt_30_50 = new TLegend(0.23, 0.71, 0.56, 0.93);
-    SetLegend(legv1oddDiffPt_30_50, 17);
+    TLegend * legv1oddDiffPt_30_50 = new TLegend(0.24, 0.06, 0.96, 0.28);
+    SetLegend(legv1oddDiffPt_30_50, 18);
     legv1oddDiffPt_30_50->AddEntry(vnA[0][16][12][0],"HF+ only","lp");
     legv1oddDiffPt_30_50->AddEntry(vnB[0][16][12][0],"HF- only","lp");
     legv1oddDiffPt_30_50->AddEntry(vnAB[0][16][12][0],Form("%s",AnalNames[16].data()),"lp");
-    legv1oddDiffPt_30_50->Draw();
+    // legv1oddDiffPt_30_50->Draw();
 
     cv1oddDiffPt_30_50->Print("figures/vN1SUB3_Pt_30_50.png","png");
 
