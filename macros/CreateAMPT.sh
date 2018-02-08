b4c549fa10a5c86f51ec4a0da4524d2b745053d2
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-02-07 23:41:36 -0600

    Update

--- macros/CreateAMPT.sh
+++ macros/CreateAMPT.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCm22SUB2",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCp22SUB2",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCm22SUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCp22SUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
 
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCm22SUB3",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCp22SUB3",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCm22SUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1MCp22SUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
 
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1SUB2",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1SUB3",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1SUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1SUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
 
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N112SUB2",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N112SUB3",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N112SUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N112SUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
 
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N2SUB2",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N2SUB3",-1.6,1.6,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N2SUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N2SUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
