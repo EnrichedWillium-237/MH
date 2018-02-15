18ccfba7a7f888a0209a4fb810d85cfd6906997e
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-02-14 23:31:36 -0600

    Update

--- macros/getSystematics.C
+++ macros/getSystematics.C
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-static const int ncbins = 13;
+static const int ncbins = 14;
 static const int cmin[]={0, 5,10,15,20,25,30,35,40,50,60,  0,20, 60};
 static const int cmax[]={5,10,15,20,25,30,35,40,50,60,70, 20,60,100};
 
@@ -17,8 +17,8 @@ TH1D * v1evenErr[ncbins];
 
 void getSystematics() {
 
-    double oddErr[ncbins] = {0.14, 0.14, 0.12, 0.11, 0.10, 0.09, 0.07, 0.06, 0.06, 0.05, 0.05, 0.07};
-    double evenErr[ncbins] = {0.08, 0.06, 0.05, 0.05, 0.05, 0.05, 0.06, 0.06, 0.05, 0.06, 0.16, 0.06};
+    double oddErr[ncbins] = {0.14, 0.14, 0.12, 0.11, 0.10, 0.09, 0.07, 0.06, 0.06, 0.05, 0.05, 0.13, 0.07};
+    double evenErr[ncbins] = {0.11, 0.09, 0.08, 0.08, 0.08, 0.07, 0.09, 0.09, 0.07, 0.09, 0.23, 0.06, 0.08};
 
     fout = new TFile("data_systematics.root","recreate");
     dirOdd = (TDirectory *) fout->mkdir("odd_errors");
