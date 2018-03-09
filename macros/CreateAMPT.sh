d44ea9b50487c9f686cf653e8ff6a1ac8930d1d5
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-03-08 18:42:44 -0600

    Update

--- macros/CreateAMPT.sh
+++ macros/CreateAMPT.sh
@@ -1,21 +1,47 @@
 #!/bin/sh
 
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",-2.4,-0.4,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",-2.4,0.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",0.4,-2.4,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",0.0,2.4,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFgSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
 
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
-#
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",-2.4,-0.4,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",-2.4,0.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",0.4,-2.4,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1EVENSUB3",0.0,2.4,true,-1,3,-0.05,0.05,-0.02,0.02)'
-#
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
-root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFgSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1ASUB2",-2.0,0.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1BSUB2",0.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1ASUB3",-2.0,0.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1BSUB3",0.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB3",-1.6,-1.2,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB3",-1.6,-1.2,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB3",-1.6,-1.2,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB3",-1.6,-1.2,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFgSUB3",-1.6,-1.2,true,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFcSUB3",1.2,1.6,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFdSUB3",1.2,1.6,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFeSUB3",1.2,1.6,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFfSUB3",1.2,1.6,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFgSUB3",1.2,1.6,true,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1fSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1fSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1gSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1gSUB2",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1fSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1fSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1gSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1gSUB3",-2.0,2.0,false,-1,3,-0.05,0.05,-0.02,0.02)'
+
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1fSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1fSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFm1gSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
+root -l -b -q 'GetVN.C+("../PbPb_AMPT.root","N1HFp1gSUB3",-2.0,2.0,true,-1,3,-0.05,0.05,-0.02,0.02)'
+
+mv vnPlots.root PbPb_AMPT_hists.root
