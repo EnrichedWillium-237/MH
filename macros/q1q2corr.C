4200fdc6a9aeddaf6f60a4899a82dc8ee2c927fb
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-03-12 14:39:59 -0500

    Update

--- macros/q1q2corr.C
+++ macros/q1q2corr.C
@@ -11,7 +11,7 @@
 #include "src/HiEvtPlaneList.h"
 
 TFile * tf = new TFile("../MH.root","read");
-//TFile * tf = new TFile("../PbPb_AMPT.root","read");
+// TFile * tf = new TFile("../PbPb_AMPT.root","read");
 // TBrowser br;
 using namespace hi;
 
@@ -20,8 +20,10 @@ void q1q2corr( string cent="25_30" ) {
     TH2D * res12cnt = (TH2D *) tf->Get(Form("vnanalyzer/Resolutions/%s/rescnt12",cent.data()));
     res12ep->Divide(res12cnt);
     //res12ep->Draw();
-    TH1D * prjp = res12ep->ProjectionX("prjp",HFp2g-HFm2+1,HFp2g-HFm2+1);
-    TH1D * prjm = res12ep->ProjectionX("prjm",HFm2g-HFm2+1,HFm2g-HFm2+1);
+    // TH1D * prjp = res12ep->ProjectionX("prjp",HFp2g-HFm2+1,HFp2g-HFm2+1);
+    // TH1D * prjm = res12ep->ProjectionX("prjm",HFm2g-HFm2+1,HFm2g-HFm2+1);
+    TH1D * prjp = res12ep->ProjectionX("prjp",trackp222-HFm2+1,trackp222-HFm2+1);
+    TH1D * prjm = res12ep->ProjectionX("prjm",trackm222-HFm2+1,trackm222-HFm2+1);
     TCanvas * c = new TCanvas(Form("c_%s",cent.data()), Form("c_%s",cent.data()), 620, 600);
     c->cd();
     //prj->Draw();
@@ -41,7 +43,7 @@ void q1q2corr( string cent="25_30" ) {
     gp->SetMarkerColor(kBlue);
     gp->RemovePoint(0);
     gp->RemovePoint(gp->GetN()-1);
-    TGraph * gm = new TGraph(12,xm,ym);
+    TGraph * gm = new TGraph(12, xm, ym);
     gm->SetMarkerStyle(20);
     gm->SetMarkerSize(1.2);
     gm->SetMarkerColor(kRed);
@@ -58,8 +60,10 @@ void q1q2corr( string cent="25_30" ) {
     TLegend * leg = new TLegend(0.2, 0.2, 0.4, 0.4);
     leg->SetBorderSize(0);
     leg->SetHeader(Form("%s",cent.data()));
-    leg->AddEntry(gp,"Q_{2} (4<#eta<5)","lp");
-    leg->AddEntry(gm,"Q_{2} (-5<#eta<-4)","lp");
+    // leg->AddEntry(gp,"Q_{2} (4<#eta<5)","lp");
+    // leg->AddEntry(gm,"Q_{2} (-5<#eta<-4)","lp");
+    leg->AddEntry(gp,"Q_{2} (2<#eta<2.4)","lp");
+    leg->AddEntry(gm,"Q_{2} (-2.4<#eta<-2)","lp");
     leg->Draw();
     c->Print(Form("q1q2/q1q2corr_%s.pdf",cent.data()),"pdf");
     c->Print(Form("q1q2/q1q2corr_%s.png",cent.data()),"png");
