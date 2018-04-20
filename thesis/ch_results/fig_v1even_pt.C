c1dbbfe5261c0fcf7f8f398cd6f509b293dee251
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-04-20 10:42:44 -0500

    Update

--- thesis/ch_results/fig_v1even_pt.C
+++ thesis/ch_results/fig_v1even_pt.C
@@ -15,7 +15,6 @@ using namespace std;
 static const int ncbins = 11;
 static const int cmin[] = {0,  5, 10, 15, 20, 25, 30, 35, 40, 50, 60};
 static const int cmax[] = {5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70};
-static const double ebinmid[] = {-2.2, -1.8, -1.4, -1.0, -0.6, -0.2, 0.2, 0.6, 1.0, 1.4, 1.8, 2.2};
 
 void SetTPaveTxt( TPaveText * txtemplate, int txtsize ) {
   txtemplate->SetFillColor(0);
@@ -43,23 +42,37 @@ TFile * fin;
 TFile * finSyst;
 TH1D * h0;
 TH1D * h1;
+TGraphErrors * N1MCp22SUB2[ncbins];
+TGraphErrors * N1MCm22SUB2[ncbins];
 TGraphErrors * N1EVENSUB2[ncbins];
 TGraphErrors * N1HFgSUB2_syst[ncbins];
 
-const double syst_v1even_eta[ncbins] = {11.5e-4, 11.5e-4, 11.5e-4, 11.5e-4, 16.2e-4, 16.2e-4, 16.2e-4, 16.2e-4, 16.2e-4, 20.1e-4, 20.1e-4};
+const double syst_v1even_pt[ncbins] = {2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4, 2.4e-4};
 
-void fig_v1even_eta() {
+void fig_v1even_pt() {
 
     fin = new TFile("../../data/MH_hists_master.root","read");
 
+    // Average negative and positive side v1even
     for (int i = 0; i<ncbins; i++) {
-        N1EVENSUB2[i] = (TGraphErrors *) fin->Get(Form("default/N1EVENSUB2/-2.4_-0.4/%d_%d/gint",cmin[i],cmax[i]));
+        N1MCp22SUB2[i] = (TGraphErrors *) fin->Get(Form("default/N1EVENSUB2/-2.4_-0.4/%d_%d/gA",cmin[i],cmax[i]));
+        N1MCm22SUB2[i] = (TGraphErrors *) fin->Get(Form("default/N1EVENSUB2/0.4_2.4/%d_%d/gA",cmin[i],cmax[i]));
+        int num =  N1MCp22SUB2[i]->GetN();
+        double xvalA[50], xvalB[50], yvalA[50], yvalB[50], yerrA[50], yerrB[50];
+        for (int j = 0; j<num; j++) {
+            N1MCp22SUB2[i]->GetPoint(j, xvalA[j], yvalA[j]);
+            yerrA[j] = N1MCp22SUB2[i]->GetErrorY(j);
+            N1MCm22SUB2[i]->GetPoint(j, xvalB[j], yvalB[j]);
+            yerrB[j] = N1MCm22SUB2[i]->GetErrorY(j);
+
+            yvalA[j] = 0.5*(yvalA[j] + yvalB[j]);
+            yerrA[j] = 0.5*sqrt( pow(yerrA[j],2) + pow(yerrB[j],2) );
+        }
+        N1EVENSUB2[i] = new TGraphErrors(num, xvalA, yvalA, 0, yerrA);
         N1EVENSUB2[i]->SetMarkerStyle(21);
         N1EVENSUB2[i]->SetMarkerSize(1.2);
         N1EVENSUB2[i]->SetMarkerColor(kBlue);
         N1EVENSUB2[i]->SetLineColor(kBlue);
-        N1EVENSUB2[i]->RemovePoint(5);
-        N1EVENSUB2[i]->RemovePoint(5);
     }
 
 
@@ -69,8 +82,8 @@ void fig_v1even_eta() {
         int num = N1EVENSUB2[i]->GetN();
         for (int j = 0; j<num; j++) {
             N1EVENSUB2[i]->GetPoint(j, x[j], y[j]);
-            xerr[j] = 0.15;
-            ysyst[j] = syst_v1even_eta[i];
+            xerr[j] = 0.2;
+            ysyst[j] = syst_v1even_pt[i];
         }
         N1HFgSUB2_syst[i] = new TGraphErrors(num, x, y, xerr, ysyst);
         N1HFgSUB2_syst[i]->SetLineColor(kBlue-10);
@@ -81,16 +94,15 @@ void fig_v1even_eta() {
 
     TCanvas * c0 = new TCanvas("c0", "c0", 900, 650);
     c0->Divide(3,2,0,0);
-    h0 = new TH1D("h0", "", 100, -2.8, 2.8);
+    h0 = new TH1D("h0", "", 1000, 0, 8.2);
     h0->SetStats(0);
     h0->SetXTitle("#eta");
     h0->SetYTitle("v_{1}^{even}");
     h0->GetYaxis()->SetDecimals();
-    h0->GetXaxis()->SetNdivisions(508);
     h0->GetYaxis()->SetNdivisions(508);
     h0->GetXaxis()->CenterTitle();
     h0->GetYaxis()->CenterTitle();
-    h0->GetYaxis()->SetRangeUser(-0.017, 0.029);
+    h0->GetYaxis()->SetRangeUser(-0.04, 0.28);
     for (int cbin = 0; cbin<6; cbin++) {
         TPad * pad0 = (TPad *) c0->cd(cbin+1);
         if (cbin == 0 || cbin == 3) pad0->SetLeftMargin(0.22);
@@ -118,6 +130,7 @@ void fig_v1even_eta() {
             htmp->GetXaxis()->SetLabelSize(0.07);
             htmp->GetYaxis()->SetLabelOffset(0.010);
         }
+        if (cbin == 4 || cbin == 5 || cbin == 2) htmp->GetXaxis()->SetRangeUser(0.01, 8.2);
         htmp->Draw();
         N1HFgSUB2_syst[cbin]->Draw("same 2");
         N1EVENSUB2[cbin]->Draw("same p");
@@ -150,23 +163,22 @@ void fig_v1even_eta() {
     tx0_2->AddText("#eta_{C} = 0");
     tx0_2->Draw();
 
-    c0->Print("../fig_v1even_eta_0_30.pdf","pdf");
+    c0->Print("../fig_v1even_pt_0_30.pdf","pdf");
 
 
     //-- --//
 
     TCanvas * c1 = new TCanvas("c1", "c1", 900, 650);
     c1->Divide(3,2,0,0);
-    h1 = new TH1D("h1", "", 100, -2.8, 2.8);
+    h1 = new TH1D("h1", "", 1000, 0, 8.2);
     h1->SetStats(0);
     h1->SetXTitle("#eta");
     h1->SetYTitle("v_{1}^{even}");
     h1->GetYaxis()->SetDecimals();
-    h1->GetXaxis()->SetNdivisions(508);
     h1->GetYaxis()->SetNdivisions(508);
     h1->GetXaxis()->CenterTitle();
     h1->GetYaxis()->CenterTitle();
-    h1->GetYaxis()->SetRangeUser(-0.017, 0.029);
+    h1->GetYaxis()->SetRangeUser(-0.04, 0.28);
     for (int cbin = 0; cbin<5; cbin++) {
         TPad * pad0 = (TPad *) c1->cd(cbin+1);
         if (cbin == 0 || cbin == 3) pad0->SetLeftMargin(0.22);
@@ -194,6 +206,7 @@ void fig_v1even_eta() {
             htmp->GetXaxis()->SetLabelSize(0.07);
             htmp->GetYaxis()->SetLabelOffset(0.010);
         }
+        if (cbin == 4 || cbin == 5 || cbin == 2) htmp->GetXaxis()->SetRangeUser(0.01, 8.2);
         htmp->Draw();
         N1HFgSUB2_syst[cbin+6]->Draw("same 2");
         N1EVENSUB2[cbin+6]->Draw("same p");
@@ -245,6 +258,6 @@ void fig_v1even_eta() {
     tx1_2->AddText("#eta_{C} = 0");
     tx1_2->Draw();
 
-    c1->Print("../fig_v1even_eta_30_70.pdf","pdf");
+    c1->Print("../fig_v1even_pt_30_70.pdf","pdf");
 
 }
