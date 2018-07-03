52edef51090aad152dc9fdfd13ce79a411dcc89c
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-07-03 18:35:10 -0500

    Update

--- thesis/ch_systematics/fig_syst_v1odd_2SE_3SE_eta.C
+++ thesis/ch_systematics/fig_syst_v1odd_2SE_3SE_eta.C
@@ -154,7 +154,7 @@ void fig_syst_v1odd_2SE_3SE_eta() {
     h0->GetXaxis()->SetLabelOffset(0.018);
     h0->GetYaxis()->SetTitleFont(43);
     h0->GetYaxis()->SetTitleSize(22);
-    h0->GetYaxis()->SetTitleOffset(3.0);
+    h0->GetYaxis()->SetTitleOffset(2.75);
     h0->GetYaxis()->SetLabelFont(43);
     h0->GetYaxis()->SetLabelSize(16);
     h0->GetYaxis()->SetLabelOffset(0.010);
@@ -167,43 +167,165 @@ void fig_syst_v1odd_2SE_3SE_eta() {
         N1HFgSUB3[cbin]->Draw("same p");
         N1HFgSUB2[cbin]->Draw("same p");
         if (cbin == 0) {
+            TPaveText * tx0_CMS = new TPaveText(0.22, 0.86, 0.42, 0.96, "NDC");
+            SetTPaveTxt(tx0_CMS, 18);
+            tx0_CMS->AddText("#eta_{C} = 0");
+            tx0_CMS->Draw();
+
             TPaveText * tx0_c0 = new TPaveText(0.52, 0.67, 0.72, 0.77, "NDC");
             SetTPaveTxt(tx0_c0, 18);
             tx0_c0->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
             tx0_c0->Draw();
 
-            TLegend * leg0 = new TLegend(0.30, 0.04, 0.69, 0.32);
-            SetLegend(leg0, 18);
-            leg0->SetHeader("#eta_{C} = 0");
-            leg0->AddEntry(N1HFgSUB2[cbin], "2 subevent v_{1}^{odd}", "p");
-            leg0->AddEntry(N1HFgSUB3[cbin], "3 subevent v_{1}^{odd}", "p");
-            leg0->Draw();
+            // TLegend * leg0 = new TLegend(0.30, 0.04, 0.69, 0.32);
+            // SetLegend(leg0, 18);
+            // leg0->SetHeader("#eta_{C} = 0");
+            // leg0->AddEntry(N1HFgSUB2[cbin], "2 subevent v_{1}^{odd}", "p");
+            // leg0->AddEntry(N1HFgSUB3[cbin], "3 subevent v_{1}^{odd}", "p");
+            // leg0->Draw();
         }
         if (cbin == 1) {
             TPaveText * tx0_c1 = new TPaveText(0.40, 0.67, 0.60, 0.77, "NDC");
             SetTPaveTxt(tx0_c1, 18);
             tx0_c1->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
             tx0_c1->Draw();
+
+            TLegend * leg0_c1 = new TLegend(0.0, 0.86, 0.47, 0.96);
+            SetLegend(leg0_c1, 18);
+            leg0_c1->AddEntry(N1HFgSUB2[cbin], "2 subevent v_{1}^{odd}", "p");
+            leg0_c1->Draw();
         }
         if (cbin == 2) {
-            TPaveText * tx0_c2 = new TPaveText(0.40, 0.67, 0.60, 0.77, "NDC");
+            TPaveText * tx0_c2 = new TPaveText(0.37, 0.67, 0.57, 0.77, "NDC");
             SetTPaveTxt(tx0_c2, 18);
             tx0_c2->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
             tx0_c2->Draw();
+
+            TLegend * leg0_c2 = new TLegend(0.0, 0.86, 0.47, 0.96);
+            SetLegend(leg0_c2, 18);
+            leg0_c2->AddEntry(N1HFgSUB3[cbin], "3 subevent v_{1}^{odd}", "p");
+            leg0_c2->Draw();
         }
     }
     TLine * ln0 = new TLine(-2.4, 1.0, 2.4, 1.0);
     for (int cbin = 3; cbin<6; cbin++) {
-        pad0[cbin+3]->cd();
+        pad0[cbin]->cd();
         TH1D * htmp = (TH1D *) h0->Clone(Form("h0_%d",cbin));
         htmp->SetYTitle("v_{1}^{odd} Ratio: Sub3/Sub2");
         htmp->GetYaxis()->SetRangeUser(0.35, 1.65);
         htmp->Draw();
         ln0->Draw();
-        N1HFgRatio[cbin]->Draw("same p");
+        N1HFgRatio[cbin-3]->Draw("same p");
     }
 
     c0->Print("../figures/fig_syst_v1odd_2SE_3SE_ratio_eta_0_30.pdf","pdf");
+
+
+
+    TCanvas * c1 = new TCanvas("c1", "c1", 830, 580);
+    TPad * pad1[6];
+    c1->cd();
+    pad1[0] = new TPad("pad1_0", "pad1_0", 0.0, 0.5, 0.38, 1.0);
+    pad1[0]->SetLeftMargin(0.22);
+    pad1[0]->SetTopMargin(0.15);
+    pad1[0]->SetBottomMargin(0);
+    pad1[0]->SetRightMargin(0);
+    pad1[0]->Draw();
+
+    c1->cd();
+    pad1[1] = new TPad("pad1_1", "pad1_1", 0.38, 0.5, 0.69, 1.0);
+    pad1[1]->SetLeftMargin(0);
+    pad1[1]->SetRightMargin(0);
+    pad1[1]->SetTopMargin(0.15);
+    pad1[1]->SetBottomMargin(0);
+    pad1[1]->Draw();
+
+    c1->cd();
+    pad1[2] = new TPad("pad1_2", "pad1_2", 0.69, 0.5, 1.0, 1.0);
+    pad1[2]->SetLeftMargin(0);
+    pad1[2]->SetRightMargin(0.05);
+    pad1[2]->SetTopMargin(0.15);
+    pad1[2]->SetBottomMargin(0);
+    pad1[2]->Draw();
+
+    c1->cd();
+    pad1[3] = new TPad("pad1_3", "pad1_3", 0.0, 0, 0.38, 0.5);
+    pad1[3]->SetLeftMargin(0.22);
+    pad1[3]->SetRightMargin(0);
+    pad1[3]->SetTopMargin(0);
+    pad1[3]->SetBottomMargin(0.18);
+    pad1[3]->SetGrid();
+    pad1[3]->Draw();
+
+    c1->cd();
+    pad1[4] = new TPad("pad1_4", "pad1_4", 0.38, 0, 0.69, 0.5);
+    pad1[4]->SetLeftMargin(0);
+    pad1[4]->SetRightMargin(0);
+    pad1[4]->SetTopMargin(0);
+    pad1[4]->SetBottomMargin(0.18);
+    pad1[4]->SetGrid();
+    pad1[4]->Draw();
+
+    c1->cd();
+    pad1[5] = new TPad("pad1_5", "pad1_5", 0.69, 0, 1.0, 0.5);
+    pad1[5]->SetLeftMargin(0);
+    pad1[5]->SetRightMargin(0.05);
+    pad1[5]->SetTopMargin(0);
+    pad1[5]->SetBottomMargin(0.18);
+    pad1[5]->SetGrid();
+    pad1[5]->Draw();
+
+    for (int cbin = 3; cbin<6; cbin++) {
+        pad1[cbin-3]->cd();
+        TH1D * htmp = (TH1D *) h0->Clone(Form("h1_%d",cbin));
+        htmp->Draw();
+        N1HFgSUB3[cbin]->Draw("same p");
+        N1HFgSUB2[cbin]->Draw("same p");
+        if (cbin == 3) {
+            TPaveText * tx1_CMS = new TPaveText(0.22, 0.86, 0.42, 0.96, "NDC");
+            SetTPaveTxt(tx1_CMS, 18);
+            tx1_CMS->AddText("#eta_{C} = 0");
+            tx1_CMS->Draw();
+
+            TPaveText * tx1_c0 = new TPaveText(0.52, 0.67, 0.72, 0.77, "NDC");
+            SetTPaveTxt(tx1_c0, 18);
+            tx1_c0->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
+            tx1_c0->Draw();
+        }
+        if (cbin == 4) {
+            TPaveText * tx1_c1 = new TPaveText(0.40, 0.67, 0.60, 0.77, "NDC");
+            SetTPaveTxt(tx1_c1, 18);
+            tx1_c1->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
+            tx1_c1->Draw();
+
+            TLegend * leg1_c1 = new TLegend(0.0, 0.86, 0.47, 0.96);
+            SetLegend(leg1_c1, 18);
+            leg1_c1->AddEntry(N1HFgSUB2[cbin], "2 subevent v_{1}^{odd}", "p");
+            leg1_c1->Draw();
+        }
+        if (cbin == 5) {
+            TPaveText * tx1_c2 = new TPaveText(0.37, 0.67, 0.57, 0.77, "NDC");
+            SetTPaveTxt(tx1_c2, 18);
+            tx1_c2->AddText(Form("%d - %d%%",cmin[cbin],cmax[cbin]));
+            tx1_c2->Draw();
+
+            TLegend * leg1_c2 = new TLegend(0.0, 0.86, 0.47, 0.96);
+            SetLegend(leg1_c2, 18);
+            leg1_c2->AddEntry(N1HFgSUB3[cbin], "3 subevent v_{1}^{odd}", "p");
+            leg1_c2->Draw();
+        }
+    }
+    for (int cbin = 6; cbin<9; cbin++) {
+        pad1[cbin-3]->cd();
+        TH1D * htmp = (TH1D *) h0->Clone(Form("h1_%d",cbin));
+        htmp->SetYTitle("v_{1}^{odd} Ratio: Sub3/Sub2");
+        htmp->GetYaxis()->SetRangeUser(0.35, 1.65);
+        htmp->Draw();
+        ln0->Draw();
+        N1HFgRatio[cbin-3]->Draw("same p");
+    }
+
+    c1->Print("../figures/fig_syst_v1odd_2SE_3SE_ratio_eta_30_60.pdf","pdf");
 
 
 
