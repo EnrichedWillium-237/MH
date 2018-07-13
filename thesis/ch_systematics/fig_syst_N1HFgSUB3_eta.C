ce10b439c6f9040b245fbcaca0edee36a8176a3d
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-07-12 19:14:47 -0500

    Update

--- thesis/ch_systematics/fig_syst_N1HFgSUB3_eta.C
+++ thesis/ch_systematics/fig_syst_N1HFgSUB3_eta.C
@@ -40,17 +40,24 @@ TGraphErrors * g_tight[ncbins];
 TGraphErrors * g_narrow[ncbins];
 TGraphErrors * g_wide[ncbins];
 
-TGraphErrors * gd_default[ncbins];
 TGraphErrors * gd_loose[ncbins];
 TGraphErrors * gd_tight[ncbins];
 TGraphErrors * gd_narrow[ncbins];
 TGraphErrors * gd_wide[ncbins];
 
-TGraphErrors * gabs_default[ncbins];
-TGraphErrors * gabs_loose[ncbins];
-TGraphErrors * gabs_tight[ncbins];
-TGraphErrors * gabs_narrow[ncbins];
-TGraphErrors * gabs_wide[ncbins];
+TGraph * gabs_loose_gshade[ncbins];
+TGraph * gabs_loose_gmin[ncbins];
+TGraph * gabs_loose_gmax[ncbins];
+TGraph * gabs_tight_gshade[ncbins];
+TGraph * gabs_tight_gmin[ncbins];
+TGraph * gabs_tight_gmax[ncbins];
+TGraph * gabs_narrow_gshade[ncbins];
+TGraph * gabs_narrow_gmin[ncbins];
+TGraph * gabs_narrow_gmax[ncbins];
+TGraph * gabs_wide_gshade[ncbins];
+TGraph * gabs_wide_gmin[ncbins];
+TGraph * gabs_wide_gmax[ncbins];
+
 
 TGraphErrors * gint_default[ncbins];
 TGraphErrors * gint_loose[ncbins];
@@ -58,17 +65,23 @@ TGraphErrors * gint_tight[ncbins];
 TGraphErrors * gint_narrow[ncbins];
 TGraphErrors * gint_wide[ncbins];
 
-TGraphErrors * gintd_default[ncbins];
 TGraphErrors * gintd_loose[ncbins];
 TGraphErrors * gintd_tight[ncbins];
 TGraphErrors * gintd_narrow[ncbins];
 TGraphErrors * gintd_wide[ncbins];
 
-TGraphErrors * gintabs_default[ncbins];
-TGraphErrors * gintabs_loose[ncbins];
-TGraphErrors * gintabs_tight[ncbins];
-TGraphErrors * gintabs_narrow[ncbins];
-TGraphErrors * gintabs_wide[ncbins];
+TGraph * gintabs_loose_gshade[ncbins];
+TGraph * gintabs_loose_gmin[ncbins];
+TGraph * gintabs_loose_gmax[ncbins];
+TGraph * gintabs_tight_gshade[ncbins];
+TGraph * gintabs_tight_gmin[ncbins];
+TGraph * gintabs_tight_gmax[ncbins];
+TGraph * gintabs_narrow_gshade[ncbins];
+TGraph * gintabs_narrow_gmin[ncbins];
+TGraph * gintabs_narrow_gmax[ncbins];
+TGraph * gintabs_wide_gshade[ncbins];
+TGraph * gintabs_wide_gmin[ncbins];
+TGraph * gintabs_wide_gmax[ncbins];
 
 TCanvas * c_g_trackQuality[ncbins];
 TCanvas * c_g_vtxRange[ncbins];
@@ -85,14 +98,27 @@ void fig_syst_N1HFgSUB3_eta() {
         TString tag = Form("/default/N1HFgSUB3/0.0_2.0/%d_%d/systematics",cmin[cbin],cmax[cbin]);
 
         c_g_trackQuality[cbin] = (TCanvas *) fin->Get(Form("%s/g_trackQuality",tag.Data()));
-        TList * list0_0 = c_g_trackQuality[cbin]->GetListOfPrimitives();
-        TList & list0_1 = list0_0[0];
-        TPad * list0_2 = (TPad *) list0_1.At(0);
-        TList & list0_3 = list0_2->GetListOfPrimitives()[0];
+        TList * list_c =  c_g_trackQuality[cbin]->GetListOfPrimitives();
+        TList & list_c_0 = list_c[0];
+        TPad * list_c_0_pad0 = (TPad *) list_c_0.At(0);
+        TList & list_c_0_pad0_0 = list_c_0_pad0->GetListOfPrimitives()[0];
+        g_loose[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(5);
+        g_default[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(6);
+        g_tight[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(7);
 
-        g_loose[cbin] = (TGraphErrors *) list0_3.At(5);
-        g_default[cbin] = (TGraphErrors *) list0_3.At(6);
-        g_tight[cbin] = (TGraphErrors *) list0_3.At(7);
+        TPad * list_c_0_pad4 = (TPad *) list_c_0.At(4);
+        TList & list_c_0_pad4_0 = list_c_0_pad4->GetListOfPrimitives()[0];
+        gd_loose[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(2);
+        gd_tight[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(3);
+
+        TPad * list_c_0_pad5 = (TPad *) list_c_0.At(5);
+        TList & list_c_0_pad5_0 = list_c_0_pad5->GetListOfPrimitives()[0];
+        gabs_loose_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(2);
+        gabs_loose_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(3);
+        gabs_loose_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(4);
+        gabs_tight_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(5);
+        gabs_tight_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(6);
+        gabs_tight_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(7);
     }
 
     // differential results, vtx cut
@@ -101,16 +127,26 @@ void fig_syst_N1HFgSUB3_eta() {
         TString tag = Form("/default/N1HFgSUB3/0.0_2.0/%d_%d/systematics",cmin[cbin],cmax[cbin]);
 
         c_g_vtxRange[cbin] = (TCanvas *) fin->Get(Form("%s/g_vtxRange",tag.Data()));
-        TList * list0_0 = c_g_vtxRange[cbin]->GetListOfPrimitives();
-        TList & list0_1 = list0_0[0];
-        TPad * list0_2 = (TPad *) list0_1.At(0);
-        TList & list0_3 = list0_2->GetListOfPrimitives()[0];
+        TList * list_c =  c_g_vtxRange[cbin]->GetListOfPrimitives();
+        TList & list_c_0 = list_c[0];
+        TPad * list_c_0_pad0 = (TPad *) list_c_0.At(0);
+        TList & list_c_0_pad0_0 = list_c_0_pad0->GetListOfPrimitives()[0];
+        g_wide[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(5);
+        g_narrow[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(7);
 
-        g_wide[cbin] = (TGraphErrors *) list0_3.At(5);
-        g_narrow[cbin] = (TGraphErrors *) list0_3.At(7);
+        TPad * list_c_0_pad4 = (TPad *) list_c_0.At(4);
+        TList & list_c_0_pad4_0 = list_c_0_pad4->GetListOfPrimitives()[0];
+        gd_wide[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(2);
+        gd_narrow[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(3);
 
-        TPad * list0_4 = (TPad *) list0_1.At(3);
-        TList & list0_5 = list0_4->GetListOfPrimitives();
+        TPad * list_c_0_pad5 = (TPad *) list_c_0.At(5);
+        TList & list_c_0_pad5_0 = list_c_0_pad5->GetListOfPrimitives()[0];
+        gabs_wide_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(2);
+        gabs_wide_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(3);
+        gabs_wide_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(4);
+        gabs_narrow_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(5);
+        gabs_narrow_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(6);
+        gabs_narrow_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(7);
     }
 
     // integral results, track quality
@@ -119,14 +155,27 @@ void fig_syst_N1HFgSUB3_eta() {
         TString tag = Form("/default/N1HFgSUB3/0.0_2.0/%d_%d/systematics",cmin[cbin],cmax[cbin]);
 
         c_gint_trackQuality[cbin] = (TCanvas *) fin->Get(Form("%s/gint_trackQuality",tag.Data()));
-        TList * list0_0 = c_gint_trackQuality[cbin]->GetListOfPrimitives();
-        TList & list0_1 = list0_0[0];
-        TPad * list0_2 = (TPad *) list0_1.At(0);
-        TList & list0_3 = list0_2->GetListOfPrimitives()[0];
+        TList * list_c =  c_gint_trackQuality[cbin]->GetListOfPrimitives();
+        TList & list_c_0 = list_c[0];
+        TPad * list_c_0_pad0 = (TPad *) list_c_0.At(0);
+        TList & list_c_0_pad0_0 = list_c_0_pad0->GetListOfPrimitives()[0];
+        gint_loose[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(5);
+        gint_default[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(6);
+        gint_tight[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(7);
 
-        gint_loose[cbin] = (TGraphErrors *) list0_3.At(5);
-        gint_default[cbin] = (TGraphErrors *) list0_3.At(6);
-        gint_tight[cbin] = (TGraphErrors *) list0_3.At(7);
+        TPad * list_c_0_pad4 = (TPad *) list_c_0.At(4);
+        TList & list_c_0_pad4_0 = list_c_0_pad4->GetListOfPrimitives()[0];
+        gintd_loose[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(2);
+        gintd_tight[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(3);
+
+        TPad * list_c_0_pad5 = (TPad *) list_c_0.At(5);
+        TList & list_c_0_pad5_0 = list_c_0_pad5->GetListOfPrimitives()[0];
+        gintabs_loose_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(2);
+        gintabs_loose_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(3);
+        gintabs_loose_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(4);
+        gintabs_tight_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(5);
+        gintabs_tight_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(6);
+        gintabs_tight_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(7);
     }
 
     // integral results, vtx cut
@@ -134,9 +183,45 @@ void fig_syst_N1HFgSUB3_eta() {
     // for (int cbin = 0; cbin<ncbins; cbin++) {
         TString tag = Form("/default/N1HFgSUB3/0.0_2.0/%d_%d/systematics",cmin[cbin],cmax[cbin]);
 
+        c_gint_vtxRange[cbin] = (TCanvas *) fin->Get(Form("%s/gint_vtxRange",tag.Data()));
+        TList * list_c =  c_gint_vtxRange[cbin]->GetListOfPrimitives();
+        TList & list_c_0 = list_c[0];
+        TPad * list_c_0_pad0 = (TPad *) list_c_0.At(0);
+        TList & list_c_0_pad0_0 = list_c_0_pad0->GetListOfPrimitives()[0];
+        gint_wide[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(5);
+        gint_narrow[cbin] = (TGraphErrors *) list_c_0_pad0_0.At(7);
+
+        TPad * list_c_0_pad4 = (TPad *) list_c_0.At(4);
+        TList & list_c_0_pad4_0 = list_c_0_pad4->GetListOfPrimitives()[0];
+        gintd_wide[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(2);
+        gintd_narrow[cbin] = (TGraphErrors *) list_c_0_pad4_0.At(3);
+
+        TPad * list_c_0_pad5 = (TPad *) list_c_0.At(5);
+        TList & list_c_0_pad5_0 = list_c_0_pad5->GetListOfPrimitives()[0];
+        gintabs_wide_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(2);
+        gintabs_wide_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(3);
+        gintabs_wide_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(4);
+        gintabs_narrow_gshade[cbin] = (TGraph *) list_c_0_pad5_0.At(5);
+        gintabs_narrow_gmin[cbin] = (TGraph *) list_c_0_pad5_0.At(6);
+        gintabs_narrow_gmax[cbin] = (TGraph *) list_c_0_pad5_0.At(7);
     }
 
+    // TCanvas * c0 = new TCanvas("c0","c0",500,500);
+    // gintabs_loose_gshade[0]->Draw("af");
+    // gintabs_loose_gmin[0]->Draw("l");
+    // gintabs_loose_gmax[0]->Draw("l");
+    // gintabs_tight_gshade[0]->Draw("f");
+    // gintabs_tight_gmin[0]->Draw("l");
+    // gintabs_tight_gmax[0]->Draw("l");
     TCanvas * c0 = new TCanvas("c0","c0",500,500);
-    g_narrow[0]->Draw("alp");
+    gintabs_wide_gshade[0]->Draw("af");
+    gintabs_wide_gmin[0]->Draw("l");
+    gintabs_wide_gmax[0]->Draw("l");
+    gintabs_narrow_gshade[0]->Draw("f");
+    gintabs_narrow_gmin[0]->Draw("l");
+    gintabs_narrow_gmax[0]->Draw("l");
+    // gint_loose[0]->Draw("alp");
+    // gint_default[0]->Draw("lp");
+    // gint_tight[0]->Draw("lp");
 
 }
